01ef754b5cfd487aeee0a9d767646d3c7dbee730
[Hello-day] <1277087689@qq.com>
2023-06-06 12:41:37 +0800

    用户管理界面初步完善

--- src/pages/UserManage.vue
+++ src/pages/UserManage.vue
@@ -18,146 +18,132 @@
           <div class="textArea" >
 
             <div class="voteChannel">
-              <!--    现有投票-->
+
               <div class="voteNowHave"  style="overflow-y:auto" >
-                <template>
-                  <el-table
-                      :data="tableData"
-                      stripe
-                      style="width: 100% ;margin-top: 30px;overflow-y:auto">
-                    <el-table-column
-                        prop="date"
-                        label="设备号"
-                        width="180">
+
+                <div style="margin: auto">
+
+                  <el-button @click="editAll">批量编辑</el-button>
+                  <el-button @click="submit">提交</el-button>
+                  <el-button @click="addAll">批量增加</el-button>
+                  <el-button @click="delectAll">批量删除</el-button>
+                </div>
+
+                  <el-table :data="tabledatas" border @selection-change="handleSelectionChange" style="margin-top: 10px">
+                    <el-table-column type="selection"></el-table-column>
+                    <el-table-column label="用户名">
+                      <template slot-scope="scope">
+                    <span v-if="scope.row.show">
+                        <el-input size="mini" placeholder="请输入内容" v-model="scope.row.title"></el-input>
+                    </span>
+                        <span v-else>{{scope.row.title}}</span>
+                      </template>
                     </el-table-column>
-                    <el-table-column
-                        prop="name"
-                        label="设备名"
-                        width="180">
+                    <el-table-column label="密码">
+                      <template slot-scope="scope">
+                    <span v-if="scope.row.show">
+                        <el-input size="mini" placeholder="请输入内容" v-model="scope.row.text"></el-input>
+                    </span>
+                        <span v-else>{{scope.row.text}}</span>
+                      </template>
                     </el-table-column>
-                    <el-table-column
-                        prop="address"
-                        label="报废日期">
-                    </el-table-column>
-                    <el-table-column
-                        prop="people"
-                        label="批准人">
+                    <el-table-column label="操作">
+                      <template slot-scope="scope">
+                        <el-button @click="edit(scope.row,scope.$index)">{{scope.row.show?'保存':"修改密码"}}</el-button>
+                        <el-button @click="cope(scope.row,scope.$index)">复制</el-button>
+                        <el-button @click="delect(scope.$index)">删除</el-button>
+                      </template>
                     </el-table-column>
                   </el-table>
-                </template>
 
               </div>
             </div>
-
-
           </div>
-
-
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import 'animate.css'
-// eslint-disable-next-line no-unused-vars
-import axios, {Axios as request} from "axios";
+import Vue from 'vue'
 export default {
-  name: "LendContent",
-  data(){
+  data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: 'iPhone11',
-        address: '11'
-      }, {
-        date: '2016-05-04',
-        name: 'iPhone12',
-        address: '22'
-      }, {
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-01',
-        name: 'iPhone13',
-        address: '33'
-      },{
-        date: '2016-05-03',
-        name: 'iPhone14',
-        address: '44'
-      }],
-      vote:[],  //存放频道内的投票项目
-      count:0,
-      Channel: this.$route.query,
-      flagOfvoteCenter:true,
-      flagOftext:true,
-      flagOfvoteData:true,
-      flagOfvoteContent:true,
-
+      tabledatas: [],
+      multipleSelection: [],
     }
   },
-  methods:{
-    goBack() {
-      this.$router.go(-1)
-    },
-
-    list(){
-      this.request.get("/vote/list/"+this.Channel.id).then(res=>{
-        this.vote = res.data
-        console.log(this.vote)
-        console.log(this.ChannelId)
-      })
-
-    },
-
-  },
   created() {
-
-    this.list()
-  }
+    this.tabledatas = [
+      { title: 'test', text: 's111sssa' },
+      { title: 'test2', text: 'ss222ssa' },
+    ]
+    this.tabledatas.map(i => {
+      i.show = false
+      return i
+    })
+  },
+  methods: {
+    edit(row, index) {
+      row.show = row.show ? false : true
+      Vue.set(this.tabledatas, index, row)
+      // 修改后保存
+    },
+    editAll() {
+      this.tabledatas.map((i, index) => {
+        i.show = true
+        Vue.set(this.tabledatas, index, i)
+      })
+    },
+    submit() {
+      this.tabledatas.map((i, index) => {
+        i.show = false
+        Vue.set(this.tabledatas, index, i)
+      })
+    },
+    // 单个复制
+    cope(val, index) {
+      this.tabledatas.splice(index, 0,JSON.parse(JSON.stringify(val)))
+    },
+    // 单个删除
+    delect(index) {
+      this.tabledatas.splice(index, 1)
+    },
+    //批量新增
+    addAll() {
+      if (this.multipleSelection.length == 0) {
+        let list = {
+          title: "",
+          text: ""
+        }
+        this.tabledatas.push(list)
+      } else {
+        this.multipleSelection.forEach((val, index)=> {
+          this.tabledatas.splice(index, 0,JSON.parse(JSON.stringify(val)))
+        });
+      }
+    },
+    //批量删除
+    delectAll() {
+      for (let i = 0; i < this.tabledatas.length; i++) {
+        const element = this.tabledatas[i];
+        element.id = i
+      }
+      if (this.multipleSelection.length == 0) this.$message.error('请先至少选择一项')
+      this.multipleSelection.forEach(element => {
+        this.tabledatas.forEach((e, i) => {
+          if (element.id == e.id) {
+            this.tabledatas.splice(i, 1)
+          }
+        });
+      });
+    },
+    //选
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    }
+  },
 }
 
 </script>
@@ -214,10 +200,8 @@ export default {
 
 
 .voteNowHave{
-
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
   height: 90%;
   width: 100%;
@@ -225,7 +209,7 @@ export default {
   font-size: 13px;
   font-weight: bold;
   color: #464646;
-  padding: 0 15px
+  padding: 15px 15px
 }
 
 
